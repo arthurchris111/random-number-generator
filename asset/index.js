834c75f1fb5378bf38908772750ecdf068cd5dc7
christian <arthurchristian111@gmail.com>
2022-07-06 13:46:06 +0000

    function

--- asset/index.js
+++ asset/index.js
@@ -1,9 +1,9 @@
 var result = document.querySelector(".result");
 var btn = document.querySelector(".btn");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", handler);
 
+function handler() {
     result.innerHTML = Math.random() * 100;
     result.innerHTML = Math.floor(result.innerHTML) + 1;
-
-});
+}
